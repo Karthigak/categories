6bab4819621f3bbd8a382468ba66553534f50ca9
Karthigak <karthiga.k@optisolbusiness.com>
2015-07-23 12:18:02 +0530

    Initial Commit

---
+++ categories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  categories
+//
+//  Created by OBS_Macmini on 7/23/15.
+//  Copyright (c) 2015 OptisolBusinessSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ categories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  categories
+//
+//  Created by OBS_Macmini on 7/23/15.
+//  Copyright (c) 2015 OptisolBusinessSolutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
